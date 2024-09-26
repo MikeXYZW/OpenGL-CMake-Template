d3a728e1133cce004d82cc6a56d743e71f5a80e7
Mikhail Volodin <141241717+MikeXYZW@users.noreply.github.com>
2024-09-26 15:32:03 +0300

    First version
    Cleaned up the first edition

---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+cmake . -B build -G "MinGW Makefiles"
+cmake --build build
+game.exe
---
+++ src/main.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+	if( glfwInit() != GLFW_TRUE )
+	{
+        std::cout << "Failed to initialize GLFW\n";		
+        return -1;
+	}
+	
+	auto window = glfwCreateWindow(800, 600, "DEMO", NULL, NULL);
+	glfwMakeContextCurrent(window);
+
+	if( !gladLoadGLLoader((GLADloadproc)(glfwGetProcAddress)) )
+    {
+        std::cout << "Failed to initialize GLAD\n";
+    	return -2;
+    }
+
+    glfwShowWindow(window);
+	
+	glClearColor( 0.25f, 0.65f, 0.25f, 1.0f );
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
