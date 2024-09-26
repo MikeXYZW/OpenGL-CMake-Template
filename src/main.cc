eed23aab12fe712575de5ef0c501cc4bc2e6b968
Mikhail Volodin <141241717+MikeXYZW@users.noreply.github.com>
2024-09-26 15:55:55 +0300

    Updated formatting for main.cc
    All identation is tabs now

--- src/main.cc
+++ src/main.cc
@@ -1,29 +1,28 @@
 #include <iostream>
-#include <string>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
 {
-	if( glfwInit() != GLFW_TRUE )
-	{
+    if( glfwInit() != GLFW_TRUE )
+    {
         std::cout << "Failed to initialize GLFW\n";		
         return -1;
-	}
+    }
 	
-	auto window = glfwCreateWindow(800, 600, "DEMO", NULL, NULL);
-	glfwMakeContextCurrent(window);
+    auto window = glfwCreateWindow(800, 600, "DEMO", NULL, NULL);
+    glfwMakeContextCurrent(window);
 
-	if( !gladLoadGLLoader((GLADloadproc)(glfwGetProcAddress)) )
+    if( !gladLoadGLLoader((GLADloadproc)(glfwGetProcAddress)) )
     {
         std::cout << "Failed to initialize GLAD\n";
-    	return -2;
+        return -2;
     }
 
     glfwShowWindow(window);
 	
-	glClearColor( 0.25f, 0.65f, 0.25f, 1.0f );
+    glClearColor( 0.25f, 0.65f, 0.25f, 1.0f );
 
     while (!glfwWindowShouldClose(window))
     {
@@ -33,7 +32,6 @@ int main(void)
         glfwPollEvents();
     }
 
-
-	glfwDestroyWindow(window);
-	glfwTerminate();
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
